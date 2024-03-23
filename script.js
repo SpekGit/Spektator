87bf06c498cf7387862715a5cc065a3de17fc3af
Haller125 <connect@spektal.com>
2024-03-23 19:37:11 +0500

    slide in animation setup and very minor update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elements = document.querySelectorAll('.bg h1, .bg p');
+    let elements = document.querySelectorAll('.bg h1, .bg p');
     elements.forEach(function(el) {
         el.classList.add('fade-in');
     });
 });
+
