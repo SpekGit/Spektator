213daab47fa0df3685f68dad99f35f19652f3fbe
Haller125 <connect@spektal.com>
2024-03-23 20:12:53 +0500

    added sequential animation runner setup

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+let animorder = 0;
+
 document.addEventListener('DOMContentLoaded', function() {
-    let elements = document.querySelectorAll('.bg h1, .bg p');
-    elements.forEach(function(el) {
-        el.classList.add('bg-ani');
-    });
+    runAnimation();
 });
 
+
+function runAnimation() {
+    switch (animorder) {
+        case 0:
+            let elements = document.querySelectorAll('.bg h1, .bg p');
+            elements.forEach(function(el) {
+                el.classList.add('bg-ani');
+            });
+            document.addEventListener('animationend', function(e) {
+                if (e.target.classList.contains('bg-ani')) {
+                    e.target.classList.remove('bg-ani');
+                }
+                animorder = 1;
+                console.log(animorder);
+                runAnimation();
+            }, {once: true});
+            break;
+        default:
+            console.log('Animation completed');
+            break;
+    }
+}
